5528daa604fe282dfb00ada3fa6c98e944d18943
NomeAluno <EmailAluno@hdb.com.br>
2025-04-07 19:46:53 -0300

    Atualiza __init__.py 2

--- todo_project/todo_project/__init__.py
+++ todo_project/todo_project/__init__.py
@@ -13,9 +13,9 @@ load_dotenv()  # Carrega as variáveis de ambiente do .env
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)  # Importante: instance_relative_config=True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.instance_path, 'site.db')
 db = SQLAlchemy(app)
 
 login_manager = LoginManager(app)
@@ -24,6 +24,9 @@ login_manager.login_message_category = 'info'
 
 bcrypt = Bcrypt(app)
 csrf = CSRFProtect(app)  # Inicialize a proteção CSRF
+
+# Garante que o diretório 'instance' exista
+os.makedirs(app.instance_path, exist_ok=True)
 
 from . import models
 from . import routes
