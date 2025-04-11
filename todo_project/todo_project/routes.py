55320507d3a20df35f4933234cd2c0941d523990
NomeAluno <EmailAluno@hdb.com.br>
2025-04-11 19:48:07 -0300

    Atualiza routes.py

--- todo_project/todo_project/routes.py
+++ todo_project/todo_project/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect, request, jsonify
-from todo_project import app, db, bcrypt
+from . import app, db, bcrypt
 from todo_project.forms import (LoginForm, RegistrationForm, UpdateUserInfoForm,
                                     UpdateUserPassword, TaskForm, UpdateTaskForm)
 from todo_project.models import User, Task
