1fe6aafdf2562d7994f621b1b35db053ec04589e
NomeAluno <EmailAluno@hdb.com.br>
2025-04-11 21:06:31 -0300

    Conserta path test_unit.sh 2

--- scripts/test_unit.sh
+++ scripts/test_unit.sh
@@ -15,7 +15,7 @@ pip install -r requirements.txt
 pip install pytest pytest-cov
 
 # Define PYTHONPATH para que o Python encontre o pacote corretamente
-export PYTHONPATH="$PYTHONPATH:$(pwd)/todo_project"
+export PYTHONPATH="$PYTHONPATH:$(pwd)"
 
 # Executa testes unitários com relatório de cobertura
 pytest --cov=todo_project tests/unitary/ --cov-report=xml
